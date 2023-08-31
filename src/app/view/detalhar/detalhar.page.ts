c843f44c141762fde2db539b7578746badd6652e
Bruno Sampietro <b.sampietro02@gmail.com>
2023-08-31 01:55:07 -0300

    confirm alert delete

--- src/app/view/detalhar/detalhar.page.ts
+++ src/app/view/detalhar/detalhar.page.ts
@@ -37,7 +37,7 @@ export class DetalharPage implements OnInit {
       this.presentAlert("Erro", "Todos os campos são obrigatórios!");
       
     }else{
-      this.presentAlert("Sucesso", "Contato Editado!");
+      this.presentAlert("Sucesso", "Contato Editado com Sucesso!");
       let editar: Contato = new Contato(this.nome, this.telefone);
       if(this.email){
         editar.email = this.email;
@@ -52,8 +52,13 @@ export class DetalharPage implements OnInit {
   }
 
   excluir(){
+    this.presentConfirmAlert("Confirmação", "Deseja excluir o contato?", "Ao excluir o contato, não será possível recuperá-lo.")
+  }
+
+  excluirContato(){
     this.contatoService.excluir(this.indice);
     this.router.navigate(['/home'])
+    this.presentAlert("Sucesso", "Contato Excluído com Sucesso");
   }
 
   ngOnInit() {
@@ -77,8 +82,29 @@ export class DetalharPage implements OnInit {
       message: message,
       buttons: ['OK'],
     });
-
     await alert.present();
   }
 
+  async presentConfirmAlert(titulo : string, subtitulo: string, msg : string)
+  {
+    const alert = await this.alertController.create({
+    header: titulo,
+    subHeader: subtitulo,
+    message: msg,
+    buttons: [
+      {
+        text: 'Cancelar',
+        role: 'cancelar',
+        handler: ()=>{}},
+      {
+        text: 'Confirmar',
+        role: 'confirmar',
+        handler:(acao) =>{
+          this.excluirContato();
+        }
+      }
+    ],
+  })
+  await alert.present();
+  }
 }
