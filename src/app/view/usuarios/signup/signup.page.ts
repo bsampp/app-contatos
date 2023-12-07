98a818c54ff2d86bf4bd12c7593d46cd27bb2080
bsampp <b.sampietro02@gmail.com>
2023-12-07 09:13:58 -0300

    signin

--- src/app/view/usuarios/signup/signup.page.ts
+++ src/app/view/usuarios/signup/signup.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { AlertService } from 'src/app/model/services/common/alert.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +10,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
+  formCadastrar!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private alert: AlertService, private router: Router, private formBuilder: FormBuilder) {
+    this.formCadastrar = new FormGroup({
+      email: new FormControl(''),
+      senha: new FormControl(''),
+      confSenha: new FormControl('')
+    })
   }
 
+  ngOnInit() {
+    this.formCadastrar = this.formBuilder.group({
+      email: ['',[Validators.required, Validators.email]],
+      senha: ['',[Validators.required, Validators.minLength(6)]],
+      confSenha: ['',[Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  get errorControl(){
+    return this.formCadastrar.controls;
+  }
+
+  submitForm(): Boolean{
+    if(!this.formCadastrar.valid){
+      this.alert.presentAlert('Erro','Erro ao Preencher')
+      return false;
+    }else{
+      this.cadastrar();
+      return true;
+    }
+  }
+
+  private cadastrar(){
+    this.alert.presentAlert('Olá','Cadastro Realizado!')
+    this.router.navigate(['/signin']);
+  }
 }
